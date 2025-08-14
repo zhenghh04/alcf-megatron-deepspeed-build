cd139e87127993d4f744f4f787da24ed6b81791d
Huihuo Zheng <huihuo.zheng@anl.gov>
2025-08-14 13:24:25 -0500

    added option for disabling the nousersite

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 This is to build Megatron-DeepSpeed on ALCF systems.
 
 ## Conda environment
+This has to be run on compute nodes
 ```bash
+module load frameworks (or other modules)
 bash utils/build_env.sh
 ```
 This also include the code from https://github.com/argonne-lcf/Megatron-DeepSpeed.git 
--- utils/build_env.sh
+++ utils/build_env.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # Please run this on a compute node
+# I would like to disable using the user library
+export PYTHONNOUSERSITE=1
 BUILD=${BUILD:-$(date +%Y-%m-%d)}
 export WORKDIR=${WORKDIR:-"$PWD/build/$BUILD"}
+mkdir -p $WORKDIR
 export https_proxy=http://proxy.alcf.anl.gov:3128
 export http_proxy=http://proxy.alcf.anl.gov:3128
-[ -e $WORKDIR/venvs ] || (source <(curl 'https://raw.githubusercontent.com/saforem2/ezpz/refs/heads/main/src/ezpz/bin/utils.sh') && ezpz_setup_env)
-export VENV=$WORKDIR/venvs/$(ls $WORKDIR/venvs/)
-source $VENV/bin/activate
+export VIRTUAL_ENV=$WORKDIR/venvs
+[ -e $WORKDIR/venvs ] || (source <(curl 'https://raw.githubusercontent.com/saforem2/ezpz/refs/heads/main/src/ezpz/bin/utils.sh') && VIRTUAL_ENV=$WORKDIR/venvs ezpz_setup_env)
+source $VIRTUAL_ENV/bin/activate
 pip3 install --upgrade pip
 pip3 install sentencepiece
 pip3 install deepspeed
